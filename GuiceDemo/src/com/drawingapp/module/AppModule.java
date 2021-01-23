95f0a741d3b67213fc6380ed11514fb9d2310015
Akash Garg <kagarg@amazon.com>
2021-01-23 22:39:36 +0530

    added Binding annotations

--- GuiceDemo/src/com/drawingapp/module/AppModule.java
+++ GuiceDemo/src/com/drawingapp/module/AppModule.java
@@ -1,5 +1,7 @@
 package com.drawingapp.module;
 
+import com.drawingapp.annotations.ColorValue;
+import com.drawingapp.annotations.EdgeValue;
 import com.drawingapp.services.DrawShape;
 import com.drawingapp.services.DrawSquare;
 import com.google.inject.AbstractModule;
@@ -9,6 +11,8 @@ public class AppModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		bind(DrawShape.class).to(DrawSquare.class);
+		bind(String.class).annotatedWith(ColorValue.class).toInstance("Red");
+		bind(Integer.class).annotatedWith(EdgeValue.class).toInstance(40);
 	}
 
 }
